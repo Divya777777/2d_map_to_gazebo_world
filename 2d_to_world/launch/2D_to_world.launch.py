0bfc038d8f407ccb6a17571d6a316d7cdb857039
MGupta28 <divya.thakkar@petpooja.com>
2025-08-25 10:51:34 +0530

    dynamic map and world file address added

--- 2d_to_world/launch/2D_to_world.launch.py
+++ 2d_to_world/launch/2D_to_world.launch.py
@@ -1,18 +1,25 @@
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
+        DeclareLaunchArgument('map_path', default_value=''),
+        DeclareLaunchArgument('output_world', default_value=''),
+        DeclareLaunchArgument('wall_height', default_value='2.0'),
+        DeclareLaunchArgument('wall_thickness', default_value='0.1'),
+        
         Node(
             package='navigation_demo',
             executable='2d_to_world',
             name='map_to_gazebo',
             output='screen',
             parameters=[{
-                'map_path': '/home/divya/project/src/navigation_demo/maps/office_2.yaml',
-                'output_world': '/home/divya/maps/genrated.world',
-                'wall_height': 2.0,
-                'wall_thickness': 0.1
+                'map_path': LaunchConfiguration('map_path'),
+                'output_world': LaunchConfiguration('output_world'),
+                'wall_height': LaunchConfiguration('wall_height'),
+                'wall_thickness': LaunchConfiguration('wall_thickness')
             }]
         )
     ])
